1a9d9c3d1ab61be1d1b591e6cce48f894003baad
Emiliano Chequer <chequeremiliano@gmail.com>
2023-08-13 20:01:05 -0500

    done all pages

--- index.php
+++ index.php
@@ -31,216 +31,216 @@
 
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-1/page-1.php';
+        include 'pages/type-3/page-1/page-1.php';
     ?>
         </div>
 
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-2/page-2.php';
+        include 'pages/type-3/page-2/page-2.php';
     ?>
         </div>
 
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-3/page-3.php';
+        include 'pages/type-3/page-3/page-3.php';
     ?>
         </div>
         
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-4/page-4.php';
+        include 'pages/type-3/page-4/page-4.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-5/page-5.php';
+        include 'pages/type-3/page-5/page-5.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-6/page-6.php';
-    ?>
-        </div>
-
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-7/page-7.php';
+        include 'pages/type-3/page-6/page-6.php';
     ?>
         </div>
 
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-8/page-8.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-9/page-9.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-10/page-10.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-11/page-11.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-12/page-12.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-13/page-13.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-14/page-14.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-15/page-15.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-16/page-16.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-17/page-17.php';
+        include 'pages/type-3/page-7/page-7.php';
     ?>
         </div>
 
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-18/page-18.php';
+        include 'pages/type-3/page-8/page-8.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-19/page-19.php';
+        include 'pages/type-3/page-9/page-9.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-20/page-20.php';
+        include 'pages/type-3/page-10/page-10.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-21/page-21.php';
+        include 'pages/type-3/page-11/page-11.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-22/page-22.php';
-    ?>
-        </div>
-
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-23/page-23.php';
+        include 'pages/type-3/page-12/page-12.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-24/page-24.php';
+        include 'pages/type-3/page-13/page-13.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-25/page-25.php';
+        include 'pages/type-3/page-14/page-14.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-26/page-26.php';
+        include 'pages/type-3/page-15/page-15.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-27/page-27.php';
+        include 'pages/type-3/page-16/page-16.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-28/page-28.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-29/page-29.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-30/page-30.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-31/page-31.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-32/page-32.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-33/page-33.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-34/page-34.php';
-    ?>
-        </div>
-        <div class="pdf-container">
-            <?php
-        include 'pages/type-2/page-35/page-35.php';
+        include 'pages/type-3/page-17/page-17.php';
     ?>
         </div>
 
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-36/page-36.php';
+        include 'pages/type-3/page-18/page-18.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-37/page-37.php';
+        include 'pages/type-3/page-19/page-19.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-38/page-38.php';
+        include 'pages/type-3/page-20/page-20.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-39/page-39.php';
+        include 'pages/type-3/page-21/page-21.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-22/page-22.php';
     ?>
         </div>
 
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-40/page-40.php';
+        include 'pages/type-3/page-23/page-23.php';
     ?>
         </div>
         <div class="pdf-container">
             <?php
-        include 'pages/type-2/page-41/page-41.php';
+        include 'pages/type-3/page-24/page-24.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-25/page-25.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-26/page-26.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-27/page-27.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-28/page-28.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-29/page-29.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-30/page-30.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-31/page-31.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-32/page-32.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-33/page-33.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-34/page-34.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-35/page-35.php';
+    ?>
+        </div>
+
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-36/page-36.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-37/page-37.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-38/page-38.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-39/page-39.php';
+    ?>
+        </div>
+
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-40/page-40.php';
+    ?>
+        </div>
+        <div class="pdf-container">
+            <?php
+        include 'pages/type-3/page-41/page-41.php';
     ?>
         </div>
 
